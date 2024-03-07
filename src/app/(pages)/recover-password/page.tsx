6c9e7e146655cc0b5a008639d83878a3fafd7820
APatel-AI <arpan.p.se@gmail.com>
2024-03-07 09:11:18 -0600

    created password recovery component, filtered payload css to custom css, updated login page layout and new user layout.

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -4,14 +4,47 @@ import { Metadata } from 'next'
 import { Gutter } from '../../_components/Gutter'
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import { RecoverPasswordForm } from './RecoverPasswordForm'
+import Image from 'next/image'
+import Link from 'next/link'
 
 import classes from './index.module.scss'
+import { RenderParams } from '../../_components/RenderParams'
 
 export default async function RecoverPassword() {
   return (
-    <Gutter className={classes.recoverPassword}>
-      <RecoverPasswordForm />
-    </Gutter>
+    <section className={classes.recoverPassword}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image
+            src="/logo-black.svg"
+            alt="logo"
+            width={250}
+            height={23}
+            className={classes.logo}
+          />
+        </Link>
+      </div>
+
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.param} />
+
+          <Link href="/login" className={classes.backLink}>
+            {' '}
+            <Image src="/assets/icons/arrow-left.svg" alt="left arrow" width={24} height={24} />
+
+            <p>Back</p>
+          </Link>
+
+
+          <div className={classes.formTitle}>
+            <h3>Forgot Password</h3>
+          </div>
+
+          <RecoverPasswordForm />
+        </div>
+      </div>
+    </section>
   )
 }
 
