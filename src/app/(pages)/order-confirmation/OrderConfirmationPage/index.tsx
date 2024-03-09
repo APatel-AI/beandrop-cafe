bd7b0d0b9aafa100dea762213b5af10266944261
APatel-AI <arpan.p.se@gmail.com>
2024-03-08 18:06:35 -0600

    linked stripe with checkout, created checkout component, added new products through admin

--- src/app/(pages)/order-confirmation/OrderConfirmationPage/index.tsx
+++ src/app/(pages)/order-confirmation/OrderConfirmationPage/index.tsx
@@ -44,7 +44,7 @@ export const OrderConfirmationPage: React.FC<{}> = () => {
             {`Your order has been confirmed. You will receive an email confirmation shortly. Your order ID is ${orderID}.`}
           </p>
           <div className={classes.actions}>
-            <Button href={`/orders/${orderID}`} label="View order" appearance="primary" />
+            <Button href={`/account/orders/${orderID}`} label="View order" appearance="primary" />
             <Button
               href={`${process.env.NEXT_PUBLIC_SERVER_URL}/orders`}
               label="View all orders"
